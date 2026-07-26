b843298ebd24f653a07a5e00d9f3b2d2643c1822
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-07-26 15:22:00 +0530

    [2520] [Time Beats: 100.00%] [Memory Beats: 56.13%] - LeetPush

---
+++ 2520-Count-the-Digits-That-Divide-a-Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countDigits(int num) {
+        int count=0;
+        int temp=num;
+        while(num>0){
+            int dig=num%10;
+            if(temp%dig==0)
+                count++;
+            num/=10;
+        }
+    return count;
+    }
+}
