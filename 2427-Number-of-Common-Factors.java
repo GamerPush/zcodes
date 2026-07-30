0980e2931babbc7176c13306bf2ffc05bee5c9da
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-07-30 14:58:54 +0530

    [2427] [Time Beats: 100.00%] [Memory Beats: 20.36%] - LeetPush

---
+++ 2427-Number-of-Common-Factors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=1;
+        if(a>=b){
+            for(int i=2;i<=b;i++){
+                if(a%i==0&&b%i==0)
+                    count++;
+            }
+        }
+        
+        else{
+            for(int i=2;i<a;i++){
+                if(a%i==0&&b%i==0)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
