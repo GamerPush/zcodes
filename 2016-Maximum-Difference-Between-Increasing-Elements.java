96db61e15231326b037be1c4666267645be77b26
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-08-05 15:45:44 +0530

    [2016] [Time Beats: 62.36%] [Memory Beats: 96.14%] - LeetPush

---
+++ 2016-Maximum-Difference-Between-Increasing-Elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int maxDiff=-1,min=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>min){
+                maxDiff=Math.max(maxDiff,nums[i]-min);
+            }
+            min=Math.min(nums[i],min);
+        }
+        return maxDiff;
+    }
+}
