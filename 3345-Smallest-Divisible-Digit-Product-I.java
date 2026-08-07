fffe78d608a44e4c0500d61bdd363c198eb11af1
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-08-07 13:22:48 +0530

    [3345] [Time Beats: 100.00%] [Memory Beats: 65.90%] - LeetPush

---
+++ 3345-Smallest-Divisible-Digit-Product-I.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int temp=n;
+        int product=1;
+        while(temp>0){
+            int dig=temp%10;
+            product*=dig;
+            temp/=10;
+        }
+        if(product%t==0)
+            return n;
+        return smallestNumber(n+1,t);
+    }
+}
