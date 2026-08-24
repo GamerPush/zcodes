fa0236feb037e28ccdb90cc6fbef08ece0689d52
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-08-24 10:52:13 +0530

    [709] [Time Beats: 100.00%] [Memory Beats: 51.63%] - LeetPush

--- 709-To-Lower-Case.java
+++ 709-To-Lower-Case.java
@@ -1,12 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        char[] str = s.toCharArray();
-        for(int i = 0;i<s.length();i++){
-           if(str[i] >='A' && str[i] <='Z'){
-            str[i] = (char)(str[i]+32);
-           }
-        }
-
-        return new String(str);
+        s = s.toLowerCase();
+        return s;
     }
 }
