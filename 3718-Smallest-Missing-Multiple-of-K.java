dd67f280733684e613fa31a6ba27fbed7a9c0bf7
Pushkar Goyal <pushkargoyal24@gmail.com>
2026-08-25 15:24:01 +0530

    [3718] [Time Beats: 10.64%] [Memory Beats: 26.41%] - LeetPush

---
+++ 3718-Smallest-Missing-Multiple-of-K.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Arrays.sort(nums);
+        int count=0;
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%k==0){
+                count++;
+                list.add(nums[i]);
+            }
+        }
+        for(int i=1;i<=count+1;i++){
+            if(!(list.contains(i*k)))
+                return i*k;
+        }
+        return nums[0]+k;
+    }
+}
